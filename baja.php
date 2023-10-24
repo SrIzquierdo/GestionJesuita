a9e48a567cbcef5e047a03645df2faa5bdcd05e0
SrIzquierdo <146435002+SrIzquierdo@users.noreply.github.com>
2023-10-24 20:46:08 +0200

    Gestión de jesuita (todo)

---
+++ baja.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="stilo.css">
+    </head>
+    <body>
+        <h1>Eliminación</h1>
+        <p><a href="index.html">Inicio</a></p>
+        <form action="" method="get">
+            <p>
+                <label for="id"></label>
+                <input type="text" name="id">
+            </p>
+            <p><input type="submit"></p>
+        </form>
+        <?php
+            if(isset($_GET["id"])){
+                $Conexion = new Mysqli("localhost","2daw","1234","jesuitas");
+
+                $id=(int)$_GET["id"];
+
+                $sql = "SELECT * FROM jesuita WHERE idJesuita = ".$id.";";
+                $Resultado=$Conexion->query($sql);
+
+                if($Resultado->num_rows==0){echo "<h2>Ese jesuita no existe.</h2>";}
+                else{
+                    echo "<table><tr><th>ID</th><th>Nombre</th><th>Firma</th></tr>";
+                    while($fila = $Resultado->fetch_array()){
+                        echo "<tr><td>".$fila["idJesuita"]."</td><td>".$fila["nombre"]."</td><td>".$fila["firma"]."</td></tr>";
+                    }
+                    echo "</table>";
+
+                    echo '
+                        <form action=procesoJesuita.php method=POST>
+                            <p>¿Es este el jesuita que desea eliminar?</p>
+                            <input type=hidden name=id value="'.$id.'">
+                            <input type=hidden name="tipo" value="3">
+                            <input type=submit value=SÍ>
+                        </form>';
+                }
+                $Conexion->close();
+            }
+        ?>
+    </body>
+</html>
---
+++ modificar.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="stilo.css">
+    </head>
+    <body>
+        <h1>Modificación</h1>
+        <p><a href="index.html">Inicio</a></p>
+        <form action="" method="get">
+            <p>
+                <label for="id"></label>
+                <input type="text" name="id">
+            </p>
+            <p><input type="submit"></p>
+        </form>
+        <?php
+            if(isset($_GET["id"])){
+                $Conexion = new Mysqli("localhost","2daw","1234","jesuitas");
+
+                $id=(int)$_GET["id"];
+
+                $sql = "SELECT * FROM jesuita WHERE idJesuita = ".$id.";";
+                $Resultado=$Conexion->query($sql);
+
+                $fila=$Resultado->fetch_array();
+                if(!$fila){echo "<h2>Ese jesuita no existe.</h2>";}
+                else{
+                    echo "<table><tr><th>ID</th><th>Nombre</th><th>Firma</th></tr>";
+                    echo "<tr><td>".$fila["idJesuita"]."</td><td>".$fila["nombre"]."</td><td>".$fila["firma"]."</td></tr></table>";
+                    echo '
+                        <form action=procesoJesuita.php method=POST>
+                            <p>
+                                <label for="id">Número identificador del jesuita: </label>
+                                <input type="text" name="id" value="'.$fila["idJesuita"].'">
+                            </p>
+                            <p>
+                                <label for="nombre">Nombre completo del jesuita: </label>
+                                <input type="text" name=nombre value="'.$fila["nombre"].'">
+                            </p>
+                            <p>
+                                <label for="firma">Firma del jesuita: </label>
+                                <input type="text" name="firma" value="'.$fila["firma"].'">
+                            </p>
+                            <p>¿Es este el jesuita que desea modificar?</p>
+                            <input type=hidden name="idAntiguo" value="'.$fila["idJesuita"].'">
+                            <input type=hidden name="tipo" value="2">
+                            <input type=submit value="SÍ">
+                        </form>';
+                }
+                $Conexion->close();
+            }
+        ?>
+    </body>
+</html>
